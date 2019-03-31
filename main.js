7f3bf36da572263c8f114124a04589ef89faf6f7
Nindya Hapsari <nindyahapsariiljas@yahoo.com>
2019-04-01 00:03:41 +0200

    add jquery, more effect, first test passed

--- main.js
+++ main.js
@@ -14,8 +14,52 @@ $(document).ready(function() {
         }
     });
 
+    $('#program-lang').hover(function(){
+        $('#programming-language').animate({ opacity: '1', borderRadius: '50px'}, 100,
+        function(){$('#tools-library, #softwares, #human-language').animate({ opacity: '0.5'});}
+        );
+    }, 
+    function(){$('#programming-language, #tools-library, #softwares, #human-language').animate({ opacity: '1', borderRadius: '15px'});
+} 
+        );
+
+ $('#tools-lib').hover(function(){
+        $('#tools-library').animate({ opacity: '1', borderRadius: '50px'}, 100,
+        function(){$('#programming-language, #softwares, #human-language').animate({ opacity: '0.5'});}
+        );
+    }, 
+    function(){$('#programming-language, #tools-library, #softwares, #human-language').animate({ opacity: '1', borderRadius: '15px'});
+} 
+        );
+        
+$('#titlesoftwares').hover(function(){
+        $('#softwares').animate({ opacity: '1', borderRadius: '50px'}, 100,
+        function(){$('#programming-language, #tools-library, #human-language').animate({ opacity: '0.5'});}
+        );
+    }, 
+    function(){$('#programming-language, #tools-library, #softwares, #human-language').animate({ opacity: '1', borderRadius: '15px'});
+} 
+        );          
+
+$('#language').hover(function(){
+        $('#human-language').animate({ opacity: '1', borderRadius: '50px'}, 100,
+        function(){$('#programming-language, #tools-library, #softwares').animate({ opacity: '0.5'});}
+        );
+    }, 
+    function(){$('#programming-language, #tools-library, #softwares, #human-language').animate({ opacity: '1', borderRadius: '15px'});
+} 
+        );   
 
 
 
     
 });
+
+// , function(){
+//     $('softwares').hide(1000);
+// }, function(){
+//     $('human-language').hide(1000);
+// } )
+
+// function(){ $('#programming-language').animate({height: "0px"}, 1000);
+//     }
